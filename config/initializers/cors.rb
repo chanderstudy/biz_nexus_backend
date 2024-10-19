ec21473a39aed5895a68037feb32d4751e47052a
Prince <4449princekamboj@gmail.com>
2024-10-19 20:34:24 +0530

    Update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://nexus.vicancode.com"
+            "https://nexus.vicancode.com",
+            "https://nexus-app.netlify.app"
 
     resource "*",
       headers: :any,
