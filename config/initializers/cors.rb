a912232b2d29f2e95d98a654178f2034e6a1d98b
Prince <4449princekamboj@gmail.com>
2025-04-18 09:12:18 +0530

    Added lavable in cors #0418

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,8 +4,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "http://localhost:3001",
+            "http://127.0.0.1:3001",
             "https://nexus.vicancode.com",
-            "https://nexus-app.netlify.app"
+            "https://nexus-app.netlify.app",
+            "https://e7fa-2401-4900-5c92-516d-4093-12ea-d1f7-72d7.ngrok-free.app",
+            "https://lovable.dev"
 
     resource "*",
       headers: :any,
